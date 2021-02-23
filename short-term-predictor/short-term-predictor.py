96249ce3ba56818973a51d9b629131ce3a854728
Peter Bangert <petbangert@gmail.com>
2021-02-23 19:59:40 +0100

    short-term-predictor.py: add comments

--- short-term-predictor/short-term-predictor.py
+++ short-term-predictor/short-term-predictor.py
@@ -6,10 +6,49 @@ import json
 import numpy as np
 from datetime import datetime, timedelta
 
+"""
+Short Term Predictor
+
+Regression over configurable amount of previous time, in order to extrapolate for a section of time in the future
+1. Do Quadratic Regression
+2. Check if largest coefficient is Positive, Publish Quadratic Regression Extrapolation
+3. If Negative, Publish Linear Regression Extrapolation
+
+Spike Detection
+1. Check if New data point is beyond x * MSE of linear regression of current data set
+2. If Yes, do not add to regression data set, add to Spike set
+2.1 If spike set length is > 3, reset regression data set to spike set, clear spike set
+3. If No, Continue as usual
+
+Incoming Data, 'metric' topic kafka example:
+{
+    "eventType":"MetricReported",
+    "uuid":"87c8cac1-140d-4c99-85d6-85bbf722498d",
+    "eventType":"MetricReported",
+    "occurredOn":"2021-02-21T17:18:44Z",
+    "kafkaTopic":"covid",
+    "kafkaMessagesPerSeconds":0.005931407,
+    "kafkaMaxMessageLatency":0,
+    "recordsProcessedPerSeconds":0,
+    "networkInPerSeconds":0.0,
+    "networkOutPerSeconds":0.0,
+    "sinkHealthy":true,
+    "cpuUtilization":0.005931407,
+    "memoryUsage":0.5370938,
+    "flinkTopic":""
+}
+
+Outgoing Data, 'st_prediction' topic example message:
+{
+    "prediction": 0.006446247827380952,
+    "occurredOn": "2021-02-21T17:25:51Z"
+}
+"""
+
 
 # Globalen Variablen
 LOAD_TOPIC = 'metric'
-PREDICTION_TOPIC = 'st_prediction'
+PREDICTION_TOPIC = 'prediction'
 REBUILD_TIME_MIN = 5
 REBUILD_TIME_SEC = REBUILD_TIME_MIN * 60
 REGRESSION_WINDOW_FACTOR = 12
